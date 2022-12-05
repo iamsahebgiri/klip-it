f2181cbbe36ae26f0496a0a050c237be98f3f9ca
Saheb Giri <47132373+iamsahebgiri@users.noreply.github.com>
2022-12-05 09:40:27 +0530

    fix: unescaped chars on creation

--- js/main.js
+++ js/main.js
@@ -78,12 +78,12 @@ $(async function () {
           <div data-id=${id} class="bg-white p-6 shadow-sm sm:rounded-md w-full">
             <div>
               <div class="text-sm leading-5 text-slate-600 overflow-auto font-medium prose">
-                <pre>${linkify(content)}</pre>
+                <pre>${linkify(sanitize(content)}</pre>
               </div>
             <div class="flex items-center justify-between mt-3">
               <p class="text-sm leading-5 text-slate-500">just now</p>
               <div>
-                <button id="copy-btn" data-id="${id}" data-content="${content}" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 rounded-sm mr-2">
+                <button id="copy-btn" data-id="${id}" data-content="${sanitize(content)}" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 rounded-sm mr-2">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-slate-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
                   </svg>
